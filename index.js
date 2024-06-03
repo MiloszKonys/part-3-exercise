9971a2e5f3e89797e049fbe95b7f5aa5e2e450b3
MiloszKonys <148326476+MiloszKonys@users.noreply.github.com>
2024-06-03 20:30:07 +0200

    fix

--- index.js
+++ index.js
@@ -3,91 +3,127 @@ require('dotenv').config()
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
-app.use(express.json());
-app.use(morgan("tiny"));
-app.use(cors());
-app.use(express.static('dist'))
 const Person = require('./models/person')
 
-morgan.token('req-body',(req)=>{
-  if(req.method==='POST'){
-    return JSON.stringify(req.body);
+app.use(express.json())
+app.use(morgan('tiny'))
+app.use(cors())
+app.use(express.static('dist'))
+
+morgan.token('req-body', (req) => {
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body)
   }
-  return "";
-});
+  return ''
+})
 
 app.use(
   morgan(
-    ":method :url :status :res[content-length] - :response-time ms :req-body"
+    ':method :url :status :res[content-length] - :response-time ms :req-body'
   )
-);
+)
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
-  Person.find({}).then(persons => {
+  Person.find({}).then((persons) => {
     response.json(persons)
   })
 })
 
-app.get("/info", (req, res, next) => {
+app.get('/info', (req, res, next) => {
   Person.estimatedDocumentCount({})
     .then((count) => {
       const message =
         `<p>Phonebook has info for ${count} people</p>` +
-        `<p>${new Date()}</p>`;
-      res.send(message);
+        `<p>${new Date()}</p>`
+      res.send(message)
     })
     .catch((err) => {
-      console.error(err);
-      next(err);
-    });
-});
+      console.error(err)
+      next(err)
+    })
+})
 
 app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!body.name || !body.number) {
-    return response.status(400).json({ 
-      error: 'name or number missing' 
+    return response.status(400).json({
+      error: 'name or number missing',
     })
   }
 
   // Check for duplicate name in the database
-  Person.findOne({ name: body.name }).then(existingPerson => {
-    if (existingPerson) {
-      return response.status(400).json({
-        error: 'Name must be unique'
-      })
-    } else {
-      const person = new Person({
-        name: body.name,
-        number: body.number,
-      })
+  Person.findOne({ name: body.name })
+    .then((existingPerson) => {
+      if (existingPerson) {
+        return response.status(400).json({
+          error: 'Name must be unique',
+        })
+      } else {
+        const person = new Person({
+          name: body.name,
+          number: body.number,
+        })
 
-      person.save().then(savedPerson => {
-        response.json(savedPerson)
-      }).catch(error => next(error))
-    }
-  }).catch(error => next(error))
+        person
+          .save()
+          .then((savedPerson) => {
+            response.json(savedPerson)
+          })
+          .catch((error) => next(error))
+      }
+    })
+    .catch((error) => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const { id } = request.params
+  const { name, number } = request.body
+
+  if (!name || !number) {
+    return response.status(400).json({
+      error: 'name or number missing',
+    })
+  }
+
+  const person = {
+    name,
+    number,
+  }
+
+  Person.findByIdAndUpdate(id, person, { new: true, runValidators: true, context: 'query' })
+    .then((updatedPerson) => {
+      if (updatedPerson) {
+        response.json(updatedPerson)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch((error) => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-  Person.findById(request.params.id).then(person => {
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
-  }).catch(error => next(error))
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch((error) => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-  Person.findByIdAndDelete(request.params.id).then(result => {
-    response.status(204).end()
-  }).catch(error => next(error))
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end()
+    })
+    .catch((error) => next(error))
 })
 
 const PORT = process.env.PORT || 3001
